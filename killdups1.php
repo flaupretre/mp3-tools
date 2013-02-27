5dde999655f6327dc2b18b25432e1dd414b6cb51
Francois Laupretre <francois@tekwire.net>
2013-02-27 18:47:41 +0100

    Rename find_dups to killdups1

---
+++ killdups1.php
@@ -0,0 +1,58 @@
+<?php
+
+require('lib/mp3lib.php');
+
+//----------------------------------
+
+get_options();
+
+$path=array_shift($argv);
+
+PHO_Display::trace('Reading tree');
+
+$arbo=new CRC_Arbo($path);
+
+$dups=$arbo->find_dups();
+
+PHO_Display::trace('Analyzing');
+
+foreach($dups as $dup)
+	{
+	echo "------------------------------------------------\n";
+	foreach ($dup as $f)
+		{
+		$f->display();
+		}
+
+	// Supprime les images en double (type AlbumArt_{...)
+	
+	$all_images=true;
+	$dir=null;
+	$same_dir=true;
+	$shortest=null;
+	$shortest_len='';
+	foreach ($dup as $n => $f)
+		{
+		if (!Image::is_image_path($f->path())) $all_images=false;
+		$path=$f->path();
+		$d=dirname($path);
+		if (is_null($dir)) $dir=$d;
+		if ($d !== $dir) $same_dir=false;
+		$b=basename($path);
+		$len=strlen($b);
+		if ((is_null($shortest))||($len<$shortest_len))
+			{
+			$shortest=$n;
+			$shortest_len=$len;
+			}
+		}
+	if ($all_images && $same_dir)
+		{
+		foreach ($dup as $n => $f)
+			{
+			if ($n!=$shortest) $arbo->delete($f);
+			}
+		}
+	}
+
+?>
