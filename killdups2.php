72ae71276b6cdf0db81e0a8c1ab710672c89a6b6
Francois Laupretre <francois@tekwire.net>
2013-02-27 18:47:58 +0100

    Rename killdups to killdups2

--- killdups2.php
+++ killdups2.php
@@ -17,19 +17,19 @@ PHO_Display::trace('Reading check tree');
 
 $c_arbo=new CRC_Arbo($check_path);
 
-foreach($c_arbo->get_files() as $cid => $cfile)
+foreach($c_arbo->get_files() as $cfile)
 	{
 	$bname=strtolower(basename($cfile->path()));
 	if (($bname==='desktop.ini')||($bname==='thumbs.db'))
 		{
-		$c_arbo->delete($cid);
+		$c_arbo->delete($cfile);
 		continue;
 		}
 	$tfile=$r_arbo->find($cfile);
 	if ($tfile !== false)
 		{
 		PHO_Display::trace("* Match: \n	".$cfile->path()."\n	".$tfile->path());
-		$c_arbo->delete($cid);
+		$c_arbo->delete($cfile);
 		}
 	}
 
